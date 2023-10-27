89b0428ffb1adcafa1001cee5e8bf920cb88c6e1
lialavezzi <lia.lavezzi@to.infn.it>
2023-10-27 07:08:39 +0000

    final upload

--- day4/02-student-project/main.py
+++ day4/02-student-project/main.py
@@ -8,18 +8,16 @@ warnings.filterwarnings('ignore')
 
 @click.command()
 ## This is an example of how you can pass parameters at MFlow runtime
-#@click.option("--data-path", default="tt", type=str)
+@click.option("--data-path", default="tt", type=str)
 def workflow(data_path):
 
+    # 1st entrypoint
     train = mlflow.run(".", "train", env_manager="local") #parameters={"data_path": data_path}, env_manager="local") 
-
     train.wait()
-
     model_run_uri = mlflow.get_run(train.run_id).info.artifact_uri
-
+    # 2nd entrypoint
     evaluate = mlflow.run(".", "evaluate", parameters={"model_run_uri": "/".join([model_run_uri, 'classifier.keras'])}, env_manager="local") 
-
-    #load_raw_data_run.wait()
+    evaluate.wait()
 
 if __name__ == "__main__":
     workflow()
--- day4/MLflow/MLProjectExample/main.py
+++ day4/MLflow/MLProjectExample/main.py
@@ -4,16 +4,20 @@ import os
 import sys
 
 
+# this is our pipeline
 
 @click.command()
 @click.option("--data-url", default="tt", type=str)
 def workflow(data_url):
+    # 1st entrypoint
     download_data_run = mlflow.run(".", "download_data", parameters={"data_url": data_url}, env_manager="local") 
     download_data_run.wait()
     artifact_path = mlflow.get_run(download_data_run.run_id).info.artifact_uri
+    # 2nd entrypoint
     prepare_train_test_run = mlflow.run(".", "prepare_train_test", parameters={"csv_path": "/".join([artifact_path, 'dataset.csv'])}, env_manager="local") 
     prepare_train_test_run.wait()
     artifact_path = mlflow.get_run(prepare_train_test_run.run_id).info.artifact_uri
+    # 3rd entrypoint
     train_run = mlflow.run(".", "train", parameters={"csv_train_path": "/".join([artifact_path, 'train.csv']), "csv_test_path": "/".join([artifact_path, 'test.csv'])}, env_manager="local") 
 
 if __name__ == "__main__":
